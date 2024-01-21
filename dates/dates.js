e1d5c657124b635aeb71b2071f833c335f3cab0d
Pro-Sultan <sultanabaniks@gmail.com>
2024-01-21 02:17:03 +0100

    learnt dates

--- dates/dates.js
+++ dates/dates.js
@@ -2,4 +2,47 @@
 
 const date = new Date()
 
+const a = date.toDateString()
+
+const b = date.toISOString()
+
+const c = date.toJSON()
+
+const d = date.toLocaleDateString()
+
+const e = date.toLocaleString()
+
+const f = date.toTimeString()
+
+const g = date.toUTCString()
+
+const h = date.toLocaleTimeString()
+
+const year = date.getFullYear()
+
+const month = date.getMonth()
+
+const hour = date.getHours()
+
+const day = date.getDay()
+
+const dateof = date.getDate()
+
+
 console.log(date);
+
+console.log(a);
+
+console.log(b);
+
+console.log(d);
+
+console.log(e);
+
+console.log(f);
+
+console.log(g);
+
+console.log(h);
+
+console.log(year, month, hour, day, dateof);
