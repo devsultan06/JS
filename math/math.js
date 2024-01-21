c64d17fd164567ca7283bfcbb958f7eff995b3d9
Pro-Sultan <sultanabaniks@gmail.com>
2024-01-21 02:29:23 +0100

    learnt math functions

---
+++ math/math.js
@@ -0,0 +1,22 @@
+
+console.log(Math.PI);
+
+const num = 4.7
+
+const round = Math.round(num)
+
+const floor = Math.floor(num)
+
+const ceil = Math.ceil(num)
+
+const trunc = Math.trunc(num)
+
+const power = Math.pow(2, 3)
+
+const sqr = Math.sqrt(num)
+
+const abs = Math.abs(-4.6)
+
+const random = Math.floor(Math.random() * 100) + 1
+
+console.log(round, floor, ceil, trunc, power, sqr, abs, random)
