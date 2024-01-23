6dbbf29f3a66f70b4cc5ac7c660ff93feed7c667
Pro-Sultan <sultanabaniks@gmail.com>
2024-01-23 01:13:09 +0100

    switch

--- switch/switch.js
+++ switch/switch.js
@@ -1,1 +1,28 @@
 
+const day = new Date().getDay();
+
+switch (day) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default:
+        break;
+}
