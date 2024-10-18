058856d00536a2bbbdde9ccf0ccd979ab6a35e49
Sultan A. Dev <sultanabaniks@gmail.com>
2024-10-18 16:22:59 +0100

    Fix variable reassignment and add console logs

--- variables/variable.js
+++ variables/variable.js
@@ -1,19 +1,24 @@
-// error: const value can't be reassigned
 const b = 2;
-
-b = 5;
 
 console.log(b);
 
-var a = 2
+var a = 2;
 
-a = 8
+a = 8;
 
 console.log(a);
 
-let m = 4
+let m = 4;
 
-m = 6
+m = 6;
 
 console.log(m);
+
+let c = null;
+
+console.log(c);
+
+var d;
+
+console.log(d);
 
