a786d5da4ab90ae32301ffd8b18fed1f40b23d14
Sultan A. Dev <sultanabaniks@gmail.com>
2024-10-19 16:54:40 +0100

    recursion

--- functions/function.js
+++ functions/function.js
@@ -1,17 +1,36 @@
-
 function add(a, b) {
-    return a + b
-    
+  return a + b;
 }
 
-let x = add(4,3)
+let x = add(4, 3);
 
 console.log(x);
 
-function metre(num) {
+function metre(num = 1000) {
   return num / 100;
 }
 
 let y = metre(595);
 
+let z = metre();
+
 console.log(y);
+
+console.log(z);
+
+(async () => {
+  console.log("Hello");
+})();
+
+function getFactorial(n) {
+  if (n == 0) {
+    return 1;
+  }
+  return n * getFactorial(n - 1);
+}
+
+let n = 3;
+
+let result = getFactorial(n);
+
+console.log(result);
